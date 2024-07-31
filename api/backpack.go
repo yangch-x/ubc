2b26be8bede26599a3f2a84db17065c07434a085
yangchx <7851962+yangchx0513@user.noreply.gitee.com>
2024-07-31 17:10:43 +0800

    init

--- api/backpack.go
+++ api/backpack.go
@@ -15,9 +15,9 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-//var configFile = flag.String("f", "etc/backpack.yaml", "the config file")
+var configFile = flag.String("f", "etc/backpack.yaml", "the config file")
 
-var configFile = flag.String("f", "D:\\goproject\\UBC\\api\\etc\\backpack.yaml", "the config file")
+//var configFile = flag.String("f", "D:\\goproject\\UBC\\api\\etc\\backpack.yaml", "the config file")
 
 func main() {
 	flag.Parse()
